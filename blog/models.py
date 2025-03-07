2742bc57880573033caf205fdb6a31a2b10ccb18
Maria <mariahelenmhlc@gmail.com>
2025-03-06 23:35:27 -0300

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ from django.db import models
 from django.utils import timezone
 
 
+#criar modelo post
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
